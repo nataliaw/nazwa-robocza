415e8b8b7c7968bce5f4dae0b0710b9769ca60b3
nataliaw <natalia.wydra@MWNOOSLMACLT160.makingwaves.no>
2014-12-02 10:39:01 +0100

    setting up firebase

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,14 @@
 /**
  * Created by ewelinaczarny on 23.11.14.
  */
+
+var app = angular
+  .module('nrApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase'
+  ]).constant('FIREBASE_URL', 'https://nazwa-robocza.firebaseio.com/');
